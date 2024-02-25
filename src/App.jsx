24ab7ab84136eaa463bb715dcd7bc5d11458cb21
tamistyping <enquiretam@gmail.com>
2024-02-25 11:32:57 +0000

    header and footer complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
+// import NavBar from './components/NavBar'
+// import Footer from './components/Footer'
+
+
+// Bootstrap imports
+
+export default function App() {
+
+  return (
+    <BrowserRouter>
+      <LandingPage />
+    </BrowserRouter>
+  )
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+export default function Footer() {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        py: 1,
+        px: 2,
+        backgroundColor: "#23B5D3", // Updated background color
+        color: "white",
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+        display: "flex", // Added flex display
+        justifyContent: "space-between", // Adjusted alignment
+        alignItems: "center", // Center vertically
+      }}
+    >
+      <Container maxWidth="sm">
+        <Typography variant="body2" color="text.secondary" align="left">
+          {"Copyright © "} {new Date().getFullYear()}
+          {"."}
+        </Typography>
+      </Container>
+      <Container maxWidth="sm" align="right" sx={{ paddingRight: 4 }}>
+        <a href="https://www.linkedin.com/in/tamparlak/" target="_blank" rel="noopener noreferrer">
+          <LinkedInIcon />
+        </a>
+      </Container>
+    </Box>
+  );
+}
+
+
