5187a7f57d9fbdc0c9a5dc10b6edbbbf71a4559c
tamistyping <enquiretam@gmail.com>
2024-02-26 17:18:28 +0000

    sign in function added

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -7,17 +7,45 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useState } from "react";
 
 const defaultTheme = createTheme();
 
 export default function LoginForm() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      username: data.get("username"),
-      password: data.get("password"),
-    });
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/v1/auth/jwt/create/",
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log("User signed up successfully");
+      localStorage.setItem("token", response.data.auth_token);
+      navigate("/discover");
+    } catch (error) {
+      console.error("Error signing up: ", error.message);
+      toast.error("Failed to sign up. Please try again!");
+    }
   };
 
   return (
@@ -26,29 +54,34 @@ export default function LoginForm() {
         <CssBaseline />
         <Box
           sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-            <Typography
-              variant="h4"
-              noWrap
-              sx={{
-                p: 5,
-                mt: 4,
-                mb: 4,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "#23B5D3",
-                textDecoration: "none",
-              }}
-            >
-              you on tonight?
-            </Typography>
-          <Typography component="h1" variant="h5" sx={{ color: "#DFE0E2" }} align="left">
-          &lt;Sign in&gt;
+          <Typography
+            variant="h4"
+            noWrap
+            sx={{
+              p: 5,
+              mt: 4,
+              mb: 4,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "#23B5D3",
+              textDecoration: "none",
+            }}
+          >
+            you on tonight?
+          </Typography>
+          <Typography
+            component="h1"
+            variant="h5"
+            sx={{ color: "#DFE0E2" }}
+            align="left"
+          >
+            &lt;Sign in&gt;
           </Typography>
           <Box
             component="form"
@@ -64,6 +97,8 @@ export default function LoginForm() {
               label="Username"
               name="username"
               autoComplete="username"
+              value={formData.username}
+              onChange={handleChange}
               autoFocus
               InputLabelProps={{ style: { color: "#071013" } }}
               color="primary"
@@ -77,6 +112,8 @@ export default function LoginForm() {
               label="Password"
               type="password"
               id="password"
+              value={formData.password}
+              onChange={handleChange}
               autoComplete="current-password"
               InputLabelProps={{ style: { color: "#071013" } }}
               color="primary"
@@ -86,15 +123,13 @@ export default function LoginForm() {
               type="submit"
               fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2, color: "white", backgroundColor: '#23B5D3' }}
+              sx={{ mt: 3, mb: 2, color: "white", backgroundColor: "#23B5D3" }}
             >
               Sign In
             </Button>
             <Grid container>
               <Grid item xs></Grid>
-              <Grid item>
-               
-              </Grid>
+              <Grid item></Grid>
             </Grid>
           </Box>
         </Box>
--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -8,7 +8,7 @@ export default function FeedPage() {
   return (
     <>
       <NavBar />
-
+        
       <Footer />
     </>
   );
